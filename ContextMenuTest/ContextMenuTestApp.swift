c1edd13a2515de2b1b1967bafcd73118466560f2
jyrnan <yoeking@gmail.com>
2021-08-07 11:14:05 +0800

    Initial Commit

---
+++ ContextMenuTest/ContextMenuTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuTestApp.swift
+//  ContextMenuTest
+//
+//  Created by Yong Jin on 2021/8/7.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
