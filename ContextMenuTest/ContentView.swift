cfeac702ee38d33307ad31b688fa7dab134759f7
jyrnan <yoeking@gmail.com>
2021-08-07 13:15:19 +0800

    first commit

--- ContextMenuTest/ContentView.swift
+++ ContextMenuTest/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = RowsData()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(viewModel.rows) {row in
+                HStack {
+                    Text(row.name)
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .opacity(row.favorite ? 1 : 0)
+                }
+                .contextMenu{
+                    Button(
+                        action: {viewModel.toggleFavorite(row: row)},
+                        label: {Text(row.favorite ? "Unfavorte" : "Favorte")}
+                        ///contextMenu文字根据row内容变化
+                    )
+                }
+            }
+        }
+    }
+}
+
+struct Row: Identifiable{
+    var id:UUID = UUID()
+    var name: String = "Name"
+    var favorite: Bool = false
+}
+
+class RowsData: ObservableObject {
+    @Published var rows: [Row] = (1...10).map{Row(name: "name\($0)", favorite: Bool.random())}
+    
+    func toggleFavorite(row: Row) {
+        if let index = rows.firstIndex(where: {$0.id == row.id}) {
+            rows[index].favorite.toggle()
+        }
     }
 }
 
---
+++ ContextMenuTest/RowModel.swift
@@ -0,0 +1,9 @@
+//
+//  RowModel.swift
+//  RowModel
+//
+//  Created by Yong Jin on 2021/8/7.
+//
+
+import Foundation
+
